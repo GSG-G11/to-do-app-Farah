51623b559c2e8c0536109e4ad398a4e20af59037
farah2003 <farah.shaqoura@example.com>
2022-03-06 19:36:18 +0200

    add final

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 const app =require('./app')
 const port = app.get('port');
 app.listen(port,()=>{
-    console.log('I ammmlistening at localhost:4000')
+    console.log('I ammlistening at localhost:4000')
 })
